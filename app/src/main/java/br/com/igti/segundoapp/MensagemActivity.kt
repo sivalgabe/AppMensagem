80e2b1ee345c4ac1716490262d953699be55e6ae
sivalgabe <sival.gabriel@gmail.com>
2022-09-01 17:54:25 -0300

    adicionado botão de voltar

--- app/src/main/java/br/com/igti/segundoapp/MensagemActivity.kt
+++ app/src/main/java/br/com/igti/segundoapp/MensagemActivity.kt
@@ -2,12 +2,16 @@ package br.com.igti.segundoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
 
 class MensagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mensagem)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         receberMensagem()
     }
 
@@ -15,5 +19,12 @@ class MensagemActivity : AppCompatActivity() {
         val mensagem = intent.getStringExtra(BUNDLE_KEY)
         findViewById<TextView>(R.id.tvMensagem).text = mensagem
 
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
